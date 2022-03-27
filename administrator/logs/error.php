7befafe2f9b6be1b5e4219acfacf3315f51c7d09
Mr-pasifik19 <Mr.pasifik@gmail.com>
2022-03-27 16:48:57 +0700

    pertama

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2022-03-27 04:49:47 UTC
+#Software: Joomla! 4.1.0 Stable [ Kuamini ] 15-February-2022 09:30 GMT
+
+#Fields: datetime	priority clientip	category	message
+2022-03-27T04:49:47+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
